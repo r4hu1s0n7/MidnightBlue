23b5b562305a644c41acfd2018df03b3f37d2d83
Rupen Chitroda <rupenchitroda16@gmail.com>
2021-03-03 15:11:07 +0530

    update commit

--- MovieRecommendation/settings.py
+++ MovieRecommendation/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'midnightblue',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
