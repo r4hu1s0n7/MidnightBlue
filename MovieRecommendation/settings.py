a52ab1be64ebb5a8fea47d21a715c2741f4e255a
Rupen Chitroda <rupenchitroda16@gmail.com>
2021-03-27 20:00:52 +0530

    debug-false

--- MovieRecommendation/settings.py
+++ MovieRecommendation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sd1ewyn!vm)$yp$nu$v95^b@vd+dz*tzq2l@h@ujp%5(3mwl3a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
