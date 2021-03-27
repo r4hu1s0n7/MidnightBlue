661341e7f45eb52a008ee6739b39c5a7beba22ed
Rupen Chitroda <rupenchitroda16@gmail.com>
2021-03-28 00:47:43 +0530

    static files

--- MovieRecommendation/settings.py
+++ MovieRecommendation/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
